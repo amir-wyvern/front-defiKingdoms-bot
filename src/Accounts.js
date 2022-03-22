c43bd45d2714b2f77cf7b86caf4412e7f9f6e50d
wyvern <amirhosein_wyvern@yahoo.com>
2022-03-22 09:43:20 +0100

    update files

--- src/Accounts.js
+++ src/Accounts.js
@@ -1,4 +1,4 @@
-import React ,{useEffect , useState} from 'react';
+import React ,{useEffect ,useRef , useState} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -10,26 +10,30 @@ import Button from '@material-ui/core/Button';
 import Title from './Title';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
- 
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from "@material-ui/core/TextField";
+import IconButton from '@material-ui/core/IconButton';
+import Web3 from 'web3';
+import AddCircleOutlineTwoToneIcon from '@mui/icons-material/AddCircleOutlineTwoTone';
+
+
 const cookies = new Cookies();
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-const useStyles = makeStyles((theme) => ({
-  seeMore: {
-    marginTop: theme.spacing(3),
-  },
-}
-)
-);
 
+export default function Accounts(classes) {
 
-export default function Orders() {
-
-  
-  const [rows, setRows] = useState([]);
+  const constructorCache = cookies.get('ListAddress')
+  let lsAddr = []
+  if (constructorCache) {
+    lsAddr = constructorCache.split("|,|").map((addr) => ({'name': 'None' ,'addr':addr ,'balance':'None' ,'hero':'None' ,'onSell': 'None'}) );
+    console.log(lsAddr);
+  }
+  const [rows, setRows] = useState(lsAddr);
 
   const delAccount = index =>{
       
@@ -38,23 +42,33 @@ export default function Orders() {
       const newRow = [...rows]; 
       newRow.splice(index ,1);
       setRows(newRow);
+
       const myArray = list_address.split("|,|");
       myArray.splice(index ,1);
-      
       const newCache = myArray.join('|,|');
       cookies.set('ListAddress',newCache , {path: '/' });
+    }
+  }
+
+  const delAllAccount = index =>{
+      
+    let list_address = cookies.get('ListAddress');
+    if (list_address) {
+      const newRow = []; 
+      setRows(newRow);
+      cookies.set('ListAddress','' , {path: '/' });
     }
   }
 
 
   useEffect(() => {
     const interval = setInterval(() => {
-      let addresses = cookies.get('ListAddress')
+      let addresses = cookies.get('ListAddress')  
       if (addresses) {
-          const myArray = addresses.split("|,|");
+          const myArray = addresses.split("|,|"); 
           let json = JSON.stringify(myArray);
           let post_data={json_data:json}
-          axios.post('http://127.0.0.1:8000/list_address/' ,post_data)
+          axios.post('http://wyvernbots.com/list_address/' ,post_data) 
             .then(res => {
               setRows(res.data);
             })
@@ -64,41 +78,102 @@ export default function Orders() {
     return () => clearInterval(interval);
   }, []);
 
-  const classes = useStyles();  
 
+  const valueRef = useRef('') 
+  const AddAddressToCache = () => {
+    let result = Web3.utils.isAddress(valueRef.current.value);
+    
+    const checkExist = rows.map((n) => n.addr == valueRef.current.value).some( (n) => n == true);
+    if (result == true && checkExist){
+      alert('This address already exists!');
+      valueRef.current.value = '';
+    }else if (result == true && !checkExist) { 
+
+      const cookies2 = new Cookies();
+      let list_address = cookies2.get('ListAddress');
+      if (!list_address) {
+        list_address = valueRef.current.value;
+      }
+      else {
+        list_address = list_address +'|,|' + valueRef.current.value;
+      }
+
+      const newRow = [...rows];  
+      newRow.push({'name' : 'None' ,'addr':valueRef.current.value ,'balance':'None' ,'hero':'None' ,'onSell':'None'}) ;
+      setRows(newRow);
+      cookies2.set('ListAddress',list_address , {path: '/' });
+      valueRef.current.value = '';
+
+    }
+    else {
+      alert('This Address is not valid!');
+      valueRef.current.value = '';
+    }
+  };
+
+  const AddButton = () => (
+
+    <IconButton onClick={AddAddressToCache}>
+      <AddCircleOutlineTwoToneIcon fontSize="large" color="action" />
+    </IconButton>
+  )
   return (
     <React.Fragment>
-      <Title>Review Accounts</Title>
-      <Table size="small"> 
-      <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Address</TableCell>
-            <TableCell>Balance</TableCell>
-            <TableCell>Hreoes</TableCell>
-            <TableCell >OnSell</TableCell>
-          </TableRow>
-      </TableHead>
+      
+      <Grid container item spacing={3}>
+        <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth 
+              id="pub_address"
+              label="Public Address"
+              name="pub_address"
+              autoComplete="0x"
+              autoFocus
+              inputRef={valueRef} 
+              InputProps={{startAdornment: <AddButton />}}
+            />
+        </Grid>
+      
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Button variant="contained" size="small" className="btn-delete" color="secondary" onClick={()=> delAllAccount() } >del all account</Button>
+            <Title>Review Accounts</Title>
+            
+            <Table size="small"> 
+            <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Balance</TableCell>
+                  <TableCell>Hreoes</TableCell>
+                  <TableCell >OnSell</TableCell>
+                </TableRow>
+            </TableHead>
 
-      <TableBody>
-          {rows.map((row, index) => {
-            return ( <TableRow >
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.addr}</TableCell>
-              <TableCell>{row.balance}</TableCell>
-              <TableCell>{row.hero}</TableCell>
-              <TableCell>{row.onSell}</TableCell>
-              <Button variant="contained" size="small" className="btn-info" color="primary" >more</Button>
-              <Button  variant="contained" size="small" className="btn-delete" color="secondary" onClick={()=> delAccount(index) }>del</Button>
-            </TableRow>) })}
-        </TableBody> 
-      </Table>
+            <TableBody>
+                {rows.map((row, index) => {
+                  return ( <TableRow >
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.addr.slice(0,4) + '***' +row.addr.slice(-4) }</TableCell>
+                    <TableCell>{row.balance}</TableCell>
+                    <TableCell>{row.hero}</TableCell>
+                    <TableCell>{row.onSell}</TableCell>
+                    <Button variant="contained" size="small" className="btn-info" color="primary" >more</Button>
+                    <Button  variant="contained" size="small" className="btn-delete" color="secondary" onClick={()=> delAccount(index) }>del</Button>
+                  </TableRow>) })}
+              </TableBody> 
+            </Table>
 
-      <div className={classes.seeMore}>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          See more orders
-        </Link>
-      </div>
+            <div className={classes.seeMore}>
+              <Link color="primary" href="#" onClick={preventDefault}>
+                See more Accounts
+              </Link>
+            </div>
+          </Paper>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,46 +10,14 @@ import Accounts  from './Accounts';
 import TextField from "@material-ui/core/TextField";
 import AddCircleOutlineTwoToneIcon from '@mui/icons-material/AddCircleOutlineTwoTone';
 import Cookies from 'universal-cookie';
-import Web3 from 'web3';
+import Web3 from 'web3';  
 
 export default function Dashboard(classes) {
   
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-  const valueRef = useRef('') 
-
-  const AddAddressToCache = () => {
-    let result = Web3.utils.isAddress(valueRef.current.value);
-    
-    if (result == true) { 
-
-      const cookies2 = new Cookies();
-      let list_address = cookies2.get('ListAddress');
-      if (!list_address) {
-        list_address = valueRef.current.value;
-      }
-      else {
-        list_address = list_address +'|,|' + valueRef.current.value;
-      }
-
-      cookies2.set('ListAddress',list_address , {path: '/' });
-      valueRef.current.value = '';
-
-    }
-    else {
-      alert('This Address is not valid!');
-      valueRef.current.value = '';
-    }
-
-  };
-
-  const AddButton = () => (
-
-      <IconButton onClick={AddAddressToCache}>
-        <AddCircleOutlineTwoToneIcon fontSize="large" color="action" />
-      </IconButton>
-    )
+  
   return (
-    
+
     <main className={classes.content}>
     <div className={classes.appBarSpacer}/>
     <Container maxWidth="lg" className={classes.container}>
@@ -69,30 +37,9 @@ export default function Dashboard(classes) {
         </Grid> 
         </Grid> 
 
-      <Grid container item spacing={1}>
-
-        <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="pub_address"
-              label="Public Address"
-              name="pub_address"
-              autoComplete="0x"
-              autoFocus
-              inputRef={valueRef} 
-              InputProps={{startAdornment: <AddButton />}}
-            />
-        </Grid>
-
-      </Grid>
-
       <Grid container item spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <Accounts  />
-          </Paper>
+            {Accounts(classes)}
         </Grid>
       </Grid>
 
